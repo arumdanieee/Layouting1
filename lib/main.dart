9680f3f9da519c29099fd053ab28014cbb8b2cdf
FlutLab.io <>
2022-04-04 22:01:56 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -301,7 +301,7 @@ class Gallery extends StatelessWidget {
                   Image.asset(
                     "assets/images/bp.jpg",
                     height: 200.0,
-                    width: 800.0,
+                    width: 400.0,
                     fit: BoxFit.cover,
                   ),
                   SizedBox(height: 5.0),
